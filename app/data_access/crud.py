9d69c094f8d82a9b4c61dab4d97897b785feec5e
Mehrdad <alex.badamtchi@gmail.com>
2023-02-04 05:32:36 +0330

    GC 008

--- app/data_access/crud.py
+++ app/data_access/crud.py
@@ -1,18 +1,21 @@
 from sqlalchemy.orm import Session
 from . import models
 
+
 class MessageRepository:
     def __init__(self, db:Session):
         self.db = db
-    
-    def get_all_messages(self):
-        return self.db.query(models.Messages).all()
 
-    def get_all_msgs(self, user):
-        return self.db.query(models.Messages).filter(models.Messages.receiver == user).all()
-    
-    def get_messages(self, **filters):
-        return self.db.query(models.Messages).filter(getattr(models.Messages, attr) == value for attr, value in filters.items()).all()
-    
+    def get_query(self, filters):
+        if type(filters) == tuple:
+            query = None
+        elif type(filters) == dict:
+            query = self.db.query(models.Messages).filter((getattr(models.Messages, attr) == value for attr, value in filters.items()))
+        return query
+
     def inbox(self, user):
-        return self.get_messages(filters={"receiver": user})
+        return self.get_query(filters={'receiver': user}).all()
+
+
+
+#getattr(models.Messages, attr) == value for attr, value in filters.items()
