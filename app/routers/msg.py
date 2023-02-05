8a3c1d53bda41f42196ef3e13e4cfa00580fe16c
Mehrdad <alex.badamtchi@gmail.com>
2023-02-05 04:58:56 +0330

    2nd generation

--- app/routers/msg.py
+++ app/routers/msg.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
 from sqlalchemy.orm import Session
 from typing import List
+import uuid
+from ..data_access import models
 from .. import schemas
 from ..data_access.database import get_db
 from ..data_access.crud import MessageRepository
@@ -22,3 +24,9 @@ def get_inbox(db:Session=Depends(get_db), user="9121143071"):
 def get_inbox(db:Session=Depends(get_db), user="9121143071"):
     msgs = MessageRepository(db).outbox(user)
     return msgs
+
+@router.post('/', response_model=list[schemas.MessageBack])
+def send_msg(msg: schemas.MessageCreate, db: Session=Depends(get_db), user="9121726429"):
+    new_id = uuid.uuid1()
+    new_msg = models.Messages(id=new_id, sender=user, *msg.dict())
+    return new_msg
